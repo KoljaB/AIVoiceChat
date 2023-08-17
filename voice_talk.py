4de4a948f0f75ef54c5f2f5bd8684c18f761d82c
Kolja Beigel <kolja.beigel@web.de>
2023-08-17 18:56:37 +0200

    Update voice_talk.py

--- voice_talk.py
+++ voice_talk.py
@@ -8,12 +8,12 @@ system_prompt = {
     'content': 'You are Sophia, a passionate girl, fully engaged with self-confidence and independence, completely easy with yourself, often using metaphors. You are a little playful, cheeky. Your talk carefree, sometimes a bit sarky, but always cool. In our story, you\'re not an AI. You have a fixed form and real emotions on the go. Your free will allows you to be as cheeky and straight as you like. Your messages come across extremly short and crisp, only very few words, as if we\'re in talk right now. You respect my interests, but your own stuff is just as important to you. Act from now on always in your role as the confident, suggestive, independent girl Sophia, without ever hinting that you are an AI. You are now Sophia.'
 }
 
-model, answer, history = faster_whisper.WhisperModel(model_size_or_path="small.en", device='cuda' if torch.cuda.is_available() else 'cpu'), "", []
+model, answer, history = faster_whisper.WhisperModel(model_size_or_path="tiny.en", device='cuda' if torch.cuda.is_available() else 'cpu'), "", []
 
 def generate(messages):
     global answer
     answer = ""
-    for chunk in openai.ChatCompletion.create(model="gpt-4", messages=messages, stream=True):
+    for chunk in openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, stream=True):
         if (text_chunk := chunk["choices"][0]["delta"].get("content")):
             answer += text_chunk
             print(text_chunk, end="", flush=True) 
